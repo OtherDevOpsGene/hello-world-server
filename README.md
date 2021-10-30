b837e0b9560119969cd20679d657b3bfbe9e17f0
Gene Gotimer <eugene.gotimer@steampunk.com>
2021-10-30 16:42:15 -0400

    Initial version

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # hello-world-spring-boot
+
+A sample application that uses Spring Boot to generate an executable jar.
+
+## Running
+
+```shell
+mvn spring-boot:run
+```
+
+This will compile and run the Spring Boot application as a server,
+listening to port 8080.
+
+<kbd>Ctrl</kbd><kbd>C</kbd> to quit.
+
+## Mappings
+
+* `/` will return "Hello, World!" and the current time.
+* `/actuator/health` will return a JSON response with the up/down status.
---
+++ src/main/java/com/steampunk/helloworld/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.steampunk.helloworld;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+  @GetMapping("/")
+  public String hello() {
+    final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
+    final LocalDateTime now = LocalDateTime.now();
+
+    return "Hello, World! The current time is " + dtf.format(now);
+  }
+}
